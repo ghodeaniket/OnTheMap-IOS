70e3b7943f9537224024d584155ee03a09e188da
aniketghode <aniket.ghode@gmail.com>
2017-04-05 16:41:29 +0530

    New XCode Project
    Blank XCode project.

---
+++ OnTheMap/OnTheMap/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  OnTheMap
+//
+//  Created by Aniket Ghode on 4/5/17.
+//  Copyright © 2017 Aniket Ghode. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
